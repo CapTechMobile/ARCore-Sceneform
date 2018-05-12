8a9be5f9b44edae3771e3698494d130b84573bb4
teegarcs <teegarcs@gmail.com>
2018-05-12 09:48:37 -0400

    Initial commit

---
+++ app/src/main/java/com/captech/ar/MainActivity.java
@@ -0,0 +1,283 @@
+package com.captech.ar;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.PixelCopy;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.Frame;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.core.Trackable;
+import com.google.ar.core.TrackingState;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.ArSceneView;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.math.Quaternion;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.Renderable;
+import com.google.ar.sceneform.rendering.ViewRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ArFragment mFragment;
+    private GestureDetector mGestureDetector;
+    private ImageView postImageView;
+    private FloatingActionButton fab;
+    private int selectedId = -1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fab = findViewById(R.id.fab);
+        postImageView = findViewById(R.id.postIcon);
+
+        mFragment = (ArFragment)
+                getSupportFragmentManager().findFragmentById(R.id.sceneform_fragment);
+
+
+        //on tapping of the scene, we want to interact with the world
+        mFragment.getArSceneView().getScene().setOnTouchListener((hitTestResult, motionEvent) -> mGestureDetector.onTouchEvent(motionEvent));
+
+        mGestureDetector =
+                new GestureDetector(
+                        this,
+                        new GestureDetector.SimpleOnGestureListener() {
+                            @Override
+                            public boolean onSingleTapUp(MotionEvent e) {
+                                tapAddObject(e);
+                                return true;
+                            }
+
+                            @Override
+                            public boolean onDown(MotionEvent e) {
+                                return true;
+                            }
+                        });
+
+
+        //take a photo on clicking of the fab
+        fab.setOnClickListener(view -> takePhoto());
+
+        postImageView.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.postIcon:
+                if (selectedId == R.id.postIcon) {
+                    //remove selection
+                    selectedId = -1;
+                    postImageView.setBackground(null);
+                } else {
+                    //selecting a post it note
+                    selectedId = R.id.postIcon;
+                    postImageView.setBackground(getDrawable(R.drawable.icon_outline));
+                }
+                break;
+        }
+
+    }
+
+
+    /**
+     * Method that takes the user's tap event and creates an anchor from it
+     * to attach a renderable post it note.
+     *
+     * @param motionEvent
+     */
+    private void tapAddObject(MotionEvent motionEvent) {
+        Frame frame = mFragment.getArSceneView().getArFrame();
+
+        if (selectedId == -1 || motionEvent == null || frame == null ||
+                frame.getCamera().getTrackingState() != TrackingState.TRACKING)
+            return;
+
+
+        for (HitResult hit : frame.hitTest(motionEvent)) {
+            Trackable trackable = hit.getTrackable();
+            if ((trackable instanceof Plane &&
+                    ((Plane) trackable).isPoseInPolygon(hit.getHitPose()))) {
+                //set the 3d model to the anchor
+                buildRenderable(mFragment, hit.createAnchor());
+
+                //remove selected item after a successful set.
+                selectedId = -1;
+                postImageView.setBackground(null);
+                break;
+
+            }
+        }
+    }
+
+
+    /**
+     * Method to build the renderable post it note.
+     *
+     * @param fragment
+     * @param anchor
+     */
+    private void buildRenderable(ArFragment fragment, Anchor anchor) {
+        ModelRenderable.builder()
+                .setSource(fragment.getContext(), Uri.parse("post_it.sfb"))
+                .build()
+                .thenAccept(renderable -> addNodeToScene(fragment, anchor, renderable))
+                .exceptionally((throwable -> {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setMessage(throwable.getMessage())
+                            .setTitle("Codelab error!");
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                    return null;
+                }));
+    }
+
+
+    /**
+     * Method to take a renderable and attach it to the anchor point the user selected.
+     *
+     * @param fragment
+     * @param anchor
+     * @param renderable
+     */
+    private void addNodeToScene(ArFragment fragment, Anchor anchor, Renderable renderable) {
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        TransformableNode node = new TransformableNode(fragment.getTransformationSystem());
+        node.setRenderable(renderable);
+        node.setParent(anchorNode);
+
+        //rotate the post it to stick to the flat surface.
+        node.setLocalRotation(new Quaternion(.65f, 0f, 0f, -.5f));
+
+        //add text view node
+        ViewRenderable.builder().setView(this, R.layout.post_it_text).build()
+                .thenAccept(viewRenderable -> {
+                    Node noteText = new Node();
+                    noteText.setParent(fragment.getArSceneView().getScene());
+                    noteText.setParent(node);
+                    noteText.setRenderable(viewRenderable);
+                    noteText.setLocalPosition(new Vector3(0.0f, -0.05f, 0f));
+                });
+
+
+        fragment.getArSceneView().getScene().addChild(anchorNode);
+        node.select();
+    }
+
+    /**
+     * Method used by the FAB to take a picture of what's currently on the screen.
+     */
+    private void takePhoto() {
+        final String filename = generateFilename();
+        ArSceneView view = mFragment.getArSceneView();
+
+        // Create a bitmap the size of the scene view.
+        final Bitmap bitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(),
+                Bitmap.Config.ARGB_8888);
+
+        // Create a handler thread to offload the processing of the image.
+        final HandlerThread handlerThread = new HandlerThread("PixelCopier");
+        handlerThread.start();
+        // Make the request to copy.
+        PixelCopy.request(view, bitmap, (copyResult) -> {
+            if (copyResult == PixelCopy.SUCCESS) {
+                try {
+                    saveBitmapToDisk(bitmap, filename);
+                } catch (IOException e) {
+                    Toast toast = Toast.makeText(MainActivity.this, e.toString(),
+                            Toast.LENGTH_LONG);
+                    toast.show();
+                    return;
+                }
+                Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content),
+                        "Photo saved", Snackbar.LENGTH_LONG);
+                snackbar.setAction("Open in Photos", v -> {
+                    File photoFile = new File(filename);
+
+                    Uri photoURI = FileProvider.getUriForFile(MainActivity.this,
+                            MainActivity.this.getPackageName() + ".ar.codelab.name.provider",
+                            photoFile);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, photoURI);
+                    intent.setDataAndType(photoURI, "image/*");
+                    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    startActivity(intent);
+
+                });
+                snackbar.show();
+            } else {
+                Toast toast = Toast.makeText(MainActivity.this,
+                        "Failed to copyPixels: " + copyResult, Toast.LENGTH_LONG);
+                toast.show();
+            }
+            handlerThread.quitSafely();
+        }, new Handler(handlerThread.getLooper()));
+    }
+
+
+    /**
+     * Helper method to return a file name
+     *
+     * @return fileName
+     */
+    private String generateFilename() {
+        String date =
+                new SimpleDateFormat("yyyyMMddHHmmss", java.util.Locale.getDefault()).format(new Date());
+        return Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES) + File.separator + "Sceneform/" + date + "_screenshot.jpg";
+    }
+
+
+    /**
+     * Method to save the image taken of your AR experience to the disk.
+     *
+     * @param bitmap
+     * @param filename
+     * @throws IOException
+     */
+    private void saveBitmapToDisk(Bitmap bitmap, String filename) throws IOException {
+        File out = new File(filename);
+        if (!out.getParentFile().exists()) {
+            out.getParentFile().mkdirs();
+        }
+        try (FileOutputStream outputStream = new FileOutputStream(filename);
+             ByteArrayOutputStream outputData = new ByteArrayOutputStream()) {
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputData);
+            outputData.writeTo(outputStream);
+            outputStream.flush();
+            outputStream.close();
+        } catch (IOException ex) {
+            throw new IOException("Failed to save bitmap to disk", ex);
+        }
+    }
+
+}
